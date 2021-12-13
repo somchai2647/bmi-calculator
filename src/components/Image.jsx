8d6b7c230b7d4bb48aa3ea5f5aab44793a010746
Somchai <somchai.b@cvc.ac.th>
2021-12-13 14:52:09 +0700

    add IF

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,7 +5,7 @@ export default function Image({ BMI }) {
 
     return (
         <div>
-            {BMI ? <img src={`/assets/images/${BMI}.png`} alt="body" width={500} /> : null}
+            {BMI ? <img src={`./assets/images/${BMI}.png`} alt="body" width={500} /> : null}
         </div>
     )
 }
